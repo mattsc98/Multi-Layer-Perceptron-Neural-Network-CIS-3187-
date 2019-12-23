582ffa217f3816067fdc13c018d0d1c975fc0df0
Matthew Schembri <mattsc98@gmail.com>
2019-12-23 14:41:56 +0100

    Set to begin graph plot

--- NeuralNet.py
+++ NeuralNet.py
@@ -5,11 +5,13 @@ import random
 learnRate = 0.2  #η
 errorThresh = 0.2  #μ
 
-inp = np.array([0,1,0,1,1])
-target = np.array([0,1,1])
+#inp = np.array([0,1,0,1,1])
+#tar = np.array([0, 1, 1])
 
-weightHid = np.zeros((4, 3), np.float)
-weightInp = np.zeros((5, 4), np.float)
+# weightHid = np.zeros((4, 3), np.float)
+# weightInp = np.zeros((5, 4), np.float)
+weightInp = np.random.uniform(-1, 1, (5, 4))
+weightHid = np.random.uniform(-1, 1, (4, 3))
 
 dataset = [["00000",     "011"],
            ["00001",     "010"],
@@ -81,11 +83,29 @@ training = [["00000",     "011"],
             ["11111",     "100"]
 ]
 
-# def setInputs():
-#     input = ([])
-#     for x in range(28):
-#         input = training[x]
-#         print("- " + input)
+
+def setInputs(data, input, target):
+    # print("a")
+    for x in range(len(data)):
+        temp = list(data[x][0])
+        temp_list = []
+        for i in range(5):
+            temp_list.append(int(temp[i]))
+        input.append(temp_list)
+        #print("inp - " + str(input[x]))
+
+        temp2 = list(data[x][1])
+        temp_list2 = []
+        for i in range(3):
+            temp_list2.append(int(temp2[i]))
+        target.append(temp_list2)
+        #print("tar - " + str(target[x]))
+
+    # print("Input: ")
+    # print(input)
+    # print("Target: ")
+    # print(target)
+    return input, target
 
 def Feedfoward(inp, target, weightInp, weightHid):
 
@@ -103,23 +123,23 @@ def Feedfoward(inp, target, weightInp, weightHid):
         errList[i] = np.subtract(target[i], outO[i])
         i += 1
 
-    print("INPUT\n", inp)
-    print("TARGET\n", target)
-    print("NETH\n", netH)
-    print("OUTH\n", outH)
-    print("NETO\n", netO)
-    print("OUTO\n", outO)
-    print("ERRORLIST\n", errList)
+    # print("INPUT\n", inp)
+    # print("TARGET\n", target)
+    # print("NETH\n", netH)
+    # print("OUTH\n", outH)
+    # print("NETO\n", netO)
+    # print("OUTO\n", outO)
+    # print("ERRORLIST\n", errList)
     #print("TESTLIST\n", testList)
 
-    backProb(outO, outH, target)
 
-    return outO, outH, errList
+    #backProb(outO, outH, target, inp)
+    return outO, outH
 
 def sigmoid(x):
     return 1/(1 + np.exp(-x))
 
-def backProb(outputO, outputH, target):
+def backProb(outputO, outputH, target, inp):
 
     outputLayerDelta = (outputO)*(1-outputO)*(target-outputO)
     for i in range(4):
@@ -129,12 +149,15 @@ def backProb(outputO, outputH, target):
     for i in range(5):
         for j in range(4):
             hiddenLayerDelta = (outputH) * (1 - outputH) * addition(outputLayerDelta, weightHid[j])
+            # print(learnRate)
+            # print(hiddenLayerDelta[j])
+            # print(inp[i])
             weightInp[i][j] += learnRate*hiddenLayerDelta[j]*inp[i]
 
-    print("\n\nOUTPUTDELTA\n", outputLayerDelta)
-    print("WEIGHTHIDDEN\n", weightHid)
-    print("HIDDENDELTA\n", hiddenLayerDelta)
-    print("WEIGHTINP\n", weightInp)
+    # print("\n\nOUTPUTDELTA\n", outputLayerDelta)
+    # print("WEIGHTHIDDEN\n", weightHid)
+    # print("HIDDENDELTA\n", hiddenLayerDelta)
+    # print("WEIGHTINP\n", weightInp)
 
 
 def addition(outDelta, outputOLayer):
@@ -152,11 +175,29 @@ def addition(outDelta, outputOLayer):
 #     print(weightL)
 
 def main():
+    #weightInp = np.random.uniform(-1, 1, (5, 4))
+    #weightHid = np.random.uniform(-1, 1, (4, 3))
+    for i in range(100):
+        trainingSet()
 
-    weightInp = np.random.uniform(-1, 1, (5, 4))
-    weightHid = np.random.uniform(-1, 1, (4, 3))
+def trainingSet():
+    train_input = []
+    train_target = []
 
-    Feedfoward(inp, target, weightInp, weightHid)
+    training_input, training_target = setInputs(training, train_input, train_target)
+
+    for i in range(25):
+        outO, outH = Feedfoward(training_input[i], training_target[i], weightInp, weightHid)
+        backProb(outO, outH, training_target[i], training_input[i])
+
+def testingSet():
+    test_input = []
+    test_target = []
+
+    testing_input, testing_target = setInputs(training, test_input, test_target)
+
+    for i in range(5):
+        outO, outH = Feedfoward(testing_input[i], testing_target[i], weightInp, weightHid)
 
 # setInputs()
 #make 2 arrays, 1 5x4 and 4x3
